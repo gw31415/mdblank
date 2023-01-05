b5ef0e8c877fc3942b7fbfe8c71bdf5b340a8e55
gw31415 <git@amas.dev>
2023-01-05 21:28:22 +0900

    support for mdx

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,7 +4,17 @@ const config: GatsbyConfig = {
   siteMetadata: {
     siteName: `めもの保管庫`,
   },
-  plugins: [`gatsby-plugin-pnpm`],
+  plugins: [
+    `gatsby-plugin-pnpm`,
+    `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+  ],
 }
 
 export default config
