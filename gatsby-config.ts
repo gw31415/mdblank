ff32970262cbdbd92af2a64faeb375cf39e8a36c
gw31415 <git@amas.dev>
2023-01-08 02:21:08 +0900

    catch-links

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,6 +7,7 @@ const config: GatsbyConfig = {
   pathPrefix: `/mdblank`, // for GitHub Pages
   plugins: [
     `gatsby-plugin-pnpm`,
+    `gatsby-plugin-catch-links`,
     {
       resolve: "gatsby-plugin-mdx",
       options: {
